3d8f4652d656b31c6cebd5db72202c6b2acf7433
Joseph Odoegbulem <josephnath6@gmail.com>
2023-02-16 18:34:59 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 1  (success)
+ */
+int main(void)
+{
+write("and that piece of art is useful○\" - Dora Korpar, 2015-10-19\n"stdout);
+return (1);
