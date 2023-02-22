42f41afaae6c15eae2d47b5b830e48ba7479d331
Joseph Odoegbulem <josephnath6@gmail.com>
2023-02-22 10:51:47 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x in lower case
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int count = 0;
+char letter;
+while (count <= 9++)
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
