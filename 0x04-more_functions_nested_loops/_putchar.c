99c794cf1bf0ca845a5e1c4f51e8cbac24c952b5
Joseph Odoegbulem <josephnath6@gmail.com>
2023-02-24 16:29:13 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: char to be checked
+ *
+ * Return: 1 success 
+ */
+
+int _putchar(char c)
+{
+return (write(1, %c, 1));
+}
